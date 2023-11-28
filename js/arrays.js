c7239f1023efb84fbaa6b8a7c036722d8eacf539
Tye Covington <tycov74@gmail.com>
2023-11-27 22:58:40 -0600

    made modification

--- js/arrays.js
+++ js/arrays.js
@@ -18,6 +18,7 @@ console.log('Inside of Arrays');
 
 // Iterating Arrays
 
+// old school way with "index"
 // for (let i = 0; i < names.length; i++) {
 //     console.log(names[i]);
 // }
@@ -25,4 +26,38 @@ console.log('Inside of Arrays');
 // For...of Loop (Spicy Loop) New way of listing array of names
 /* for(let name of names)
     console.log(name);
+ */
+
+// for Each Loop
+
+/* names.forEach(name => ) {
+    console.log(name);
+
+});
+
+ */
+
+//  Spread Syntax
+/*  const someArray = [...names] ===> spread syntax; can be used during function arguments / pass in arguments
+    console.log(someArray);
+
+    names.push('Another Name');
+    console.log(names);
+
+    console.log(someArray);
+
+    function showCoordinates(lat, long) {
+    console.log(`Latitude: ${lat}`);
+    console.log(`Longitude: ${long}`);
+
+    const coordinates = [         ,       ]
+    showCoords(...coordinates); ==> spread of coordinates
+
+    function
+
+ */
+
+/**
+ *
+ *
  */
--- js/external.js
+++ js/external.js
@@ -1,26 +1,27 @@
 
 console.log ("Hello from external JavaScript")
 
-// alert("Welcome to my website");
+alert("Welcome to my website");
 
-// let userResponse = prompt("What is your favorite color")
-// console.log(`user input was ${userResponse}`)
+/*let userResponse = prompt("What is your favorite color")
+console.log(`user input was ${userResponse}`)
 
 // These two lines are the same
-// alert(`Great! ` + userColor + " is my favorite color tool");
+//alert(`Great! ` + userResponse + " is my favorite color too");
 //
 
-// let messageColor = `Great, ${userResponse} is my favorite color as well`
-// alert(messageColor)
+let messageColor = `Great, ${userResponse} is my favorite color as well`
+alert(messageColor)
+*/
 
-let littleMermaidRentalDays = prompt(`days rented`);
+/* let littleMermaidRentalDays = prompt(`days rented`);
 let brotherBearRentalDays = prompt(`days rented`)
 let herculesRentalDays = prompt(`days rented`)
 let costForEachMovie = 3
 let totalCostOfMovies = (littleMermaidRentalDays * costForEachMovie) + (brotherBearRentalDays * costForEachMovie) + (herculesRentalDays * costForEachMovie);
-totalCostOfMovies;
-alert(`total amount paid ${totalCostOfMovies}`)
-
+//totalCostOfMovies;
+alert(`total amount paid $${totalCostOfMovies.toFixed(2)}`)
+*/
 
 let googleHoursWorked = Number(prompt(`how many hours worked`));
 let facebookHoursWorked = Number(prompt(`how many hours worked`));
@@ -30,5 +31,7 @@ let facebookRate = 350
 let amazonRate = 380
 let totalAmountPaid = (googleHoursWorked * googleRate) + (facebookHoursWorked * facebookRate) + (amazonHoursWorked * amazonRate);
 totalAmountPaid;
-alert(`total amount made is ${totalAmountPaid}`)
+alert(`total amount made is $${totalAmountPaid}`)
+
+
 
